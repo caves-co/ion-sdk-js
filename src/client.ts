0cd68019e73d9751eb818545f882deee6f249ec2
Furqan Rydhan <furqan.rydhan@gmail.com>
2020-11-24 09:41:01 +0000

    fixes

--- src/client.ts
+++ src/client.ts
@@ -77,8 +77,8 @@ export default class Client extends EventEmitter {
       });
       log.info('join success: result => ' + JSON.stringify(data));
       if ('pubs' in data) {
-        for (const pub of data['pubs']) {
-          console.log('pub', pub);
+        for (const pub of data.pubs) {
+          log.info('join pub', pub);
           this.knownStreams.set(pub.mid, objToStrMap(pub.tracks));
         }
       }
